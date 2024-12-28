ee6c034b9f6452f33fdf5c7eb2a987ec2cf5413f
Davronbek <davronbek8733@gmail.com>
2024-12-28 17:54:53 +0500

    add logging

--- ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
+++ ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
@@ -1,6 +1,42 @@
 using ExpenseTracker.Web.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+if (environment == "Development")
+{
+    builder.Host.UseSerilog((context, configuration) =>
+    {
+        configuration
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day);
+    });
+}
+else
+{
+    builder.Host.UseSerilog((context, configuration) =>
+    {
+        configuration
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+        .WriteTo.ApplicationInsights(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration
+        {
+            InstrumentationKey = "82ce4973-abc0-4cba-b3fd-b3184f89a841"
+        },
+        TelemetryConverter.Traces);
+    });
+}
 
 // Add services to the container.
 builder.Services.RegisterServices(builder.Configuration);
