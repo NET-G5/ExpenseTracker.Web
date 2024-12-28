dd152b79c20491cdd2233dc8f2ca8eb7232466b7
Davronbek <davronbek8733@gmail.com>
2024-12-28 17:59:21 +0500

    add production settings

--- ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
+++ ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
@@ -1,63 +1,70 @@
 using ExpenseTracker.Web.Extensions;
 using Serilog;
 
-Log.Logger = new LoggerConfiguration()
+try
+{
+    Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
     .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Logging.ClearProviders();
 
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-if (environment == "Development")
-{
-    builder.Host.UseSerilog((context, configuration) =>
+    if (environment == "Development")
     {
-        configuration
-        .MinimumLevel.Debug()
-        .WriteTo.Console()
-        .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day);
-    });
-}
-else
-{
-    builder.Host.UseSerilog((context, configuration) =>
-    {
-        configuration
-        .MinimumLevel.Debug()
-        .WriteTo.Console()
-        .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
-        .WriteTo.ApplicationInsights(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration
+        builder.Host.UseSerilog((context, configuration) =>
         {
-            InstrumentationKey = "82ce4973-abc0-4cba-b3fd-b3184f89a841"
-        },
-        TelemetryConverter.Traces);
-    });
+            configuration
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day);
+        });
+    }
+    else
+    {
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            configuration
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+            .WriteTo.ApplicationInsights(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration
+            {
+                InstrumentationKey = "82ce4973-abc0-4cba-b3fd-b3184f89a841"
+            },
+            TelemetryConverter.Traces);
+        });
+    }
+
+    // Add services to the container.
+    builder.Services.RegisterServices(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-// Add services to the container.
-builder.Services.RegisterServices(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Error(ex.Message);
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
