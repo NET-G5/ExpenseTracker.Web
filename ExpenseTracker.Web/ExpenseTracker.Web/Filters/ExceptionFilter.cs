97f9bf48cec693dbe60c677977598b3fdd998a52
Davronbek <davronbek8733@gmail.com>
2024-12-28 18:06:43 +0500

    Update ExceptionFilter.cs

--- ExpenseTracker.Web/ExpenseTracker.Web/Filters/ExceptionFilter.cs
+++ ExpenseTracker.Web/ExpenseTracker.Web/Filters/ExceptionFilter.cs
@@ -6,8 +6,17 @@ namespace ExpenseTracker.Web.Filters;
 
 public class ExceptionFilter : IExceptionFilter
 {
+    private readonly ILogger<ExceptionFilter> _logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnException(ExceptionContext context)
     {
+        _logger.LogError(context.Exception.Message);
+
         var statusCode = context.Exception switch
         {
             HttpRequestException exception => GetStatusCode(exception.StatusCode),
