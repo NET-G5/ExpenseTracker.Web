08d09443536192391bf4b82d2d7ee397890c5f4f
Jamshidbek <161879703+jamshidbekbahtiyorivich@users.noreply.github.com>
2025-01-11 21:19:26 +0500

    ForgotPassword fixed

--- ExpenseTracker.Web/ExpenseTracker.Web/Requests/Auth/ResetPasswordRequest.cs
+++ ExpenseTracker.Web/ExpenseTracker.Web/Requests/Auth/ResetPasswordRequest.cs
@@ -1,3 +1,7 @@
 ﻿namespace ExpenseTracker.Web.Requests.Auth;
 
-public sealed record ResetPasswordRequest(string Email, string Password, string PasswordConfirm, string Token);
+public sealed record ResetPasswordRequest(
+    string Email,
+    string Token,
+    string NewPassword,
+    string ConfirmPassword);
