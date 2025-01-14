773316bed60e6f4c92a134630e95a942c98d2d63
Davronbek <davronbek8733@gmail.com>
2025-01-14 15:51:15 +0500

    remove application insights

--- ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
+++ ExpenseTracker.Web/ExpenseTracker.Web/Program.cs
@@ -8,27 +8,9 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-// builder.Logging.ClearProviders();
-
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration
-    .MinimumLevel.Debug()
-    .WriteTo.Console()
-    .WriteTo.File("logs/logs_.txt", Serilog.Events.LogEventLevel.Information, rollingInterval: RollingInterval.Day)
-    .WriteTo.File("logs/errors_.txt", Serilog.Events.LogEventLevel.Error, rollingInterval: RollingInterval.Day)
-    .WriteTo.ApplicationInsights("InstrumentationKey=82ce4973-abc0-4cba-b3fd-b3184f89a841;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=6bc8df31-e243-481c-a9b1-944e20763451", TelemetryConverter.Events);
-});
 
 // Add services to the container.
 builder.Services.RegisterServices(builder.Configuration);
-builder.Services.AddApplicationInsightsTelemetry(options =>
-{
-    options.ConnectionString = "InstrumentationKey=82ce4973-abc0-4cba-b3fd-b3184f89a841;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=6bc8df31-e243-481c-a9b1-944e20763451";
-});
-
 
 var app = builder.Build();
 
